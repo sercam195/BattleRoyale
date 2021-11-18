9fc88f1af324df39e726f0d47ff5195adcdfdf20
Sergio <sercam195@alu.centroafuera.es>
2021-11-18 14:17:26 +0100

    nuevo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 15; i++) {
-            Hilos hilo = new Hilos(false);
+            Hilos hilo = new Hilos(false,0);
             hilo.setName("Jugador " + i);
             hilo.start();
         }
@@ -17,9 +17,11 @@ public class Main {
 
 class Hilos extends Thread {
     boolean bonus;
+    int puntuacion;
 
-    Hilos(boolean bonus){
+    Hilos(boolean bonus, int puntuacion){
         this.bonus = bonus;
+        this.puntuacion = puntuacion;
     }
     @Override
     public void run() {
@@ -41,29 +43,42 @@ class Batalla{
     public static void addParticipante(Hilos hilo) throws InterruptedException {
         Random r = new Random();
         try {
-                semaphore.acquire();
-                Thread.sleep(r.nextInt(5000) + 1000);
-                if (!primero.compareAndExchange(false,true))
-                {
-                    hilo.bonus=true;
-                }
+            semaphore.acquire();
+            Thread.sleep(r.nextInt(5000) + 1000);
+            if (!primero.compareAndExchange(false,true))
+                { hilo.bonus=true; }
+
             if (cont.get()<5) {
-                System.out.println(hilo.getName()+" - "+hilo.bonus+"\n");
+                Puntuacion.calcularPuntuacion(hilo);
+                System.out.println("Hilo ganador "+hilo.getName()+" - "+hilo.bonus+" - "+hilo.puntuacion+"\n");
                 cont.getAndIncrement();
             }
-            System.out.println("5 Últimos\n");
             if (cont.get()<10 && cont.get()>=5) {
-                System.out.println(hilo.getName()+" - "+hilo.bonus+"\n");
+                System.out.println("Hilo perdedor "+hilo.getName()+" - "+hilo.bonus+" - "+hilo.puntuacion+"\n");
                 cont.getAndIncrement();
             }
             if (cont.get()==10) {
-                System.out.println("Dejo pasar 5\n");
                 semaphore.release(5);
-                cont.getAndIncrement();
-                System.out.println(hilo.getName()+" - "+hilo.bonus+"\n");
             }
+            if (cont.get()<15 && cont.get()>=10) {
+                Puntuacion.calcularPuntuacion(hilo);
+                System.out.println("Hilo sustituto "+hilo.getName()+" - "+hilo.bonus+" - "+hilo.puntuacion+"\n");
+                cont.getAndIncrement();
+            }
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+}
+class Puntuacion{
+    public static void calcularPuntuacion(Hilos hilos) {
+        Random r = new Random();
+        if (hilos.bonus){
+            hilos.puntuacion=(r.nextInt(10) + 1)*2;
+        }
+        else
+        hilos.puntuacion=r.nextInt(10) + 1;
+    }
+
 }
